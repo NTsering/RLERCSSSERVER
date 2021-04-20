7ed838cacb1a892ef71112837516164e4029af79
NTsering <ngodrup.tsering@gmail.com>
2021-04-20 15:22:21 -0400

    comments

--- executeall.sh
+++ executeall.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# cd /home/nps2dsoccer/Project/logs/dataset/shoots/RoboCup/
-# cd /home/nps2dsoccer/Project/logs/dataset/shoots/RoboCup/2019/PreliminaryRound/GroupH/20190704144102-HillStone_1-vs-MT2019_5/
-# cd /home/nps2dsoccer/Project/logs/dataset/shoots/RoboCup/2019/MainRound/GroupK/20190706093045-FRA-UNIted_1-vs-YuShan2019_1
-# cd /home/nps2dsoccer/Project/logs/dataset/shoots/RoboCup/2019/
+# To Benchmark shoot skill for 20 iteratation, execute this script. 
+
 LOG_PATH="/home/nps2dsoccer/Project/logs/dataset/shoots/RoboCup/2019/"
 RESULT_PATH="/home/nps2dsoccer/Project/logs/dataset/results"
 # echo "Path:" $PWD
@@ -11,7 +9,7 @@ count=0
 # echo "$jsonfiles"
 
 shopt -s extglob
-for i in 1 2 3 4 5 6 7 8
+for i in {1..20}
 do
     cd $LOG_PATH
     for file in **/**/**/R*; 
@@ -35,12 +33,13 @@ do
         # cd /home/nps2dsoccer/Project/rcssmonitor-16.0.0/build/src
         # ./rcssmonitor &
 
+        # Path to L-team here
         cd /home/nps2dsoccer/Project/rcsclient/receptivity/receptivity/src
-        gnome-terminal -- sh -c "./start.sh; echo $$ ; bash"
+        gnome-terminal -- sh -c "./start.sh; bash"
 
-        # cd /home/nps2dsoccer/Project/rcsclient/cyrus 
-        cd /home/nps2dsoccer/Project/rcsclient/helios
-        gnome-terminal --  sh -c "./startAll; bash"
+        # Change the path to binary for R-team here 
+        cd /home/nps2dsoccer/Project/rcsclient/yushan
+        gnome-terminal --  sh -c "./start.sh; bash"
 
         sleep 10
     
@@ -51,28 +50,26 @@ do
         PIDTOKILL=$(pgrep -i bash -n)
         echo PIDTOKILL ${PIDTOKILL}
         kill $PIDTOKILL
-        
-        # sleep 1
+
         pkill rcssserver
 
         FOLDER_NAME="${FILEPATH////-}" 
         FINAL_FOLDER_NAME="${FOLDER_NAME/.json/}"
 
-        # echo $FINAL_FOLDER_NAME
 
         
         cd $RESULT_PATH
         # mkdir -p cyrus/"$i"/"$FINAL_FOLDER_NAME"
-        mkdir -p helios/"$i"/"$FINAL_FOLDER_NAME"
+        # mkdir -p helios/"$i"/"$FINAL_FOLDER_NAME"
+        mkdir -p yushan/"$i"/"$FINAL_FOLDER_NAME"
         
         cd
-        # echo $PWD
         rm /home/nps2dsoccer/Project/rlercssserver/rcssserver-16.0.0/build/src/*.rcl
         # mv "/home/nps2dsoccer/Project/rlercssserver/rcssserver-16.0.0/build/src/"*".rcg" "$RESULT_PATH/cyrus/"$i"/"$FINAL_FOLDER_NAME"/"
-         mv "/home/nps2dsoccer/Project/rlercssserver/rcssserver-16.0.0/build/src/"*".rcg" "$RESULT_PATH/helios/"$i"/"$FINAL_FOLDER_NAME"/"
+        #  mv "/home/nps2dsoccer/Project/rlercssserver/rcssserver-16.0.0/build/src/"*".rcg" "$RESULT_PATH/helios/"$i"/"$FINAL_FOLDER_NAME"/"
+        mv "/home/nps2dsoccer/Project/rlercssserver/rcssserver-16.0.0/build/src/"*".rcg" "$RESULT_PATH/yushan/"$i"/"$FINAL_FOLDER_NAME"/"
 
     done
 done
 
 
-echo Count $count
